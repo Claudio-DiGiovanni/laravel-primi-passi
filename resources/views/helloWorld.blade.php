24df546db2c06bc431c9fc47dd9aed188598bb8c
Claudio-DiGiovanni <digioclaudio97@gmail.com>
2023-01-12 14:59:09 +0100

    hello world

---
+++ resources/views/helloWorld.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hello World</title>
+</head>
+<body>
+    <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam, error debitis? Ad blanditiis laboriosam vero, cum tenetur nam quod temporibus. Sed, ad quo. Nemo adipisci delectus, error numquam facere nobis.</p>
+</body>
+</html>
